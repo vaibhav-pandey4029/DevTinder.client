877acd4d8b98606db3fae4165f4ceb376b9ea0f0
vaibhav-pandey4029 <vaibhapandey68@gmail.com>
2025-06-10 22:28:36 +0530

    Added routing using react-router-dom

--- devTinder/src/App.jsx
+++ devTinder/src/App.jsx
@@ -1,10 +1,21 @@
-function App() {
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Body from './Body';
+import Login from './Login';
+import Profile from './Profile';
 
+function App() {
   return (
     <>
-      <h1 className="text-3xl underline">Hello world</h1>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body/>}>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
