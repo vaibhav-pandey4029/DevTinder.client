d41e1634c9489109ea43bbbc68b1624f79bdaf49
vaibhav-pandey4029 <vaibhapandey68@gmail.com>
2025-06-12 22:38:49 +0530

    redirect to login if not auth

--- devTinder/src/components/Body.jsx
+++ devTinder/src/components/Body.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../constants";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store)=>store.user);
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(
+        BASE_URL + "/profile/view",
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data));
+    } catch (error) {
+      if(error.status === 401){
+        navigate("/login");
+      }
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    // TODO: if interval server error then redirect to error page
+    // if user is unauthorized redirect to login page
+    if(!user)
+      fetchUser();
+  },[]);
   return (
     <div>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
--- devTinder/src/components/Navbar.jsx
+++ devTinder/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const user = useSelector((store)=>store.user)
   return (
     <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
         </div>
         {user && 
         <div className="flex gap-2 items-center">
@@ -29,16 +30,16 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a>Settings</a>
+                <Link>Settings</Link>
               </li>
               <li>
-                <a>Logout</a>
+                <Link>Logout</Link>
               </li>
             </ul>
           </div>
